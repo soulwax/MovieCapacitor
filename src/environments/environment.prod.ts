5c1adc6d206594b83f2f92cd2b9a3f5b6181fe37
soulwax <kling.mobil@gmail.com>
2023-04-10 11:32:05 +0200

    Resolving env variable problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
-interface ProcessEnv {
-  [key: string]: string
-}
-
-declare const process: {
-  env: ProcessEnv
-};
-
 export const environment = {
   baseUrl: 'https://api.themoviedb.org/3',
   production: false,
-  movieApiKey: process.env.MOVIE_API_KEY,
+  movieApiKey: process.env['MOVIE_API_KEY'],
   images: 'https://image.tmdb.org/t/p'
 };
