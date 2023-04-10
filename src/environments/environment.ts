75375b8f6a25ff5a75fb4d7f62911178d2dc0f13
soulwax <kling.mobil@gmail.com>
2023-04-10 11:35:36 +0200

    Resolving env variable problem 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'https://api.themoviedb.org/3',
   production: false,
-  movieApiKey: process.env['MOVIE_API_KEY'],
+  movieApiKey: "c16b79b414631d4f20f858909916f4fc",
   images: 'https://image.tmdb.org/t/p'
 };
