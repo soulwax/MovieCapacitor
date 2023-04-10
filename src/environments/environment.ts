8fa9844e56b9543d1abc3ffbbd3937e028a60768
soulwax <kling.mobil@gmail.com>
2023-04-10 11:22:06 +0200

    noPropertyAccessFromIndexSignature: false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,14 @@
+interface ProcessEnv {
+  [key: string]: string
+}
+
+declare const process: {
+  env: ProcessEnv
+};
+
 export const environment = {
   baseUrl: 'https://api.themoviedb.org/3',
   production: false,
-  movieApiKey: ["MOVIE_API_KEY"],
+  movieApiKey: process.env.MOVIE_API_KEY,
   images: 'https://image.tmdb.org/t/p'
 };
