418de094b8d8d577b6d0df877e30dd26ff4b3936
soulwax <kling.mobil@gmail.com>
2023-04-10 11:26:32 +0200

    type declaration for env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+interface ProcessEnv {
+  [key: string]: string
+}
+
+declare const process: {
+  env: ProcessEnv
+};
+
 export const environment = {
   baseUrl: 'https://api.themoviedb.org/3',
   production: false,
